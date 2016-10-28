68a13176a84390f8089c60caf4d1521d2a6997fa
JaimeXD <jaime.obando@ucsp.edu.pe>
2016-10-28 10:35:43 -0500

    Update punto.h

--- punto.h
+++ punto.h
@@ -1,36 +1,58 @@
-#ifndef PUNTO_H_INCLUDED
-#define PUNTO_H_INCLUDED
-#include "punto.cpp"
-#include <iostream>
+#include "puntoarray.h"
 
 using namespace std;
 
-void Point::push_back( Point &p){
-    int i;
-    for(i=0;*a[i]!='\0';++i)
-            ;
-    *a[i]=*p;
-    count<<*a[i]<<endl;
+Pointarray::Pointarray(){
+    this->points=new Point[0];
+    this->_size=0;
 }
-void Point::insert( int posicion){
-}
-void Point::remove( int posicion){
+Pointarray::Pointarray(Point p[],int _size){
+    this->points=new Point[_size];
     int i;
-    int *a=&p;
-    for(i=0;*a[i]!='\0';++i)
-        if(i==posicion)
-            a[i]=a[i+1];
+    for(i=0;i<_size;++i)
+        Point[i]=P[i];
 
 }
-void Point::getzide(){
+void Pointarray::push_back( Point &p){
+    int i;
+    for(i=0;i<_size;++i)
+        ;
+    points[i]=p;
+
+}
+void Pointarray::resize(int x){
+    Points *a;
+    a=new Points[x];
+    this->_size=x;
+    for (int i=0;i<x;i++)
+        a[i]=points[i];
+    delete[] points;
+    points=a;
+}
+void Pointarray::insert( int posicion,Point &p){
+    resize(_size+1);
+    for(int i=_size,i>posicion;i--)
+        points[i]=points[--1];
+    points[posicion]=p;
+}
+void Pointarray::remove( int posicion){
+    int i=posicion+1;
+    for(;i<_size;i++)
+        points[i-1]=points[i];
+    resize(_size-1)
+
+}
+void Pointarray::getzide(){
     int i;
     int a=0
     for(i=0;p[i]!='\0';++i){
         a+=1;
     }
-    cout<<"el tamaño es:"<<a<<endl;
+    cout<<"el tamaÃ±o es:"<<a<<endl;
 }
-void Point::clean(){
+void Pointarray::clean(){
+    for(int i=_size,i>posicion;i--)
+        points[i]=0;
+     resize(0);
 
 }
-#endif // PUNTO_H_INCLUDED
