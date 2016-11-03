e0e4255e691fa0e1611e671ada8ad0ac0a5b20b8
JaimeXD <jaime.obando@ucsp.edu.pe>
2016-11-03 06:23:57 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
-#include "punto.cpp"
+#include "punto.h"
+#include "puntoarray.h"
 using namespace std;
 
 int main()
 {
+    Point p;
+    Point a(1,8);
+    a.suma(4,5);
+    a.printf();
+    Pointarray();
+
+    Pointarray ( a[],5);
+
 
 
 
 
 }
+
